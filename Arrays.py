00cbeb3bb3207beaedf7d01d370df559dbb4f3c0
Devarapallivamsi <vvitvamsi@gmail.com>
2024-02-27 09:12:58 +0530

    Dutch National Flag algo

--- Arrays.py
+++ Arrays.py
@@ -292,3 +292,33 @@ lst = [2, 7, 11, 15]
 target = 9
 ans = twoSum(lst, target)
 print(ans)
+
+
+# Dutch National Flag algorithm
+# Problem Statement: Given an array consisting of only 0s, 1s, and 2s.
+# Write a program to in-place sort the array without using inbuilt sort functions.
+# (Expected: Single pass-O(N) and constant space)
+# Brute: Merge sort the array. TC: O(N*LogN)
+
+# Trace with the examples below to get a grip.
+lst2 = [1, 2, 2, 0, 1, 0, 1]
+lst = [1, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0]
+def dnfBrute(arr):
+    low = 0
+    mid = 0
+    high = len(arr) - 1
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[mid], arr[low] = arr[low], arr[mid]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            # mid += 1
+            high -= 1
+    return arr
+
+ans = dnfBrute(lst)
+print(ans)
